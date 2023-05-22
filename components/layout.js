92c219113c9b20802aa61185e4c2c70b844fed9a
Adam C <tottzi@gmail.com>
2023-05-22 08:38:24 +0200

    Add name

--- components/layout.js
+++ components/layout.js
@@ -6,8 +6,8 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
-export const siteTitle = 'Next.js Sample Website'
+const name = 'Go Balkan'
+export const siteTitle = 'GoBalkan'
 
 export default function Layout({ children, home }) {
   return (
