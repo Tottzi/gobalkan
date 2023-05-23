f452c8efde36016fa0341fc37d13063d3a0ea2c4
Adam C <tottzi@gmail.com>
2023-05-23 09:16:03 +0200

    Add basic text

--- components/layout.js
+++ components/layout.js
@@ -1,66 +1,43 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Script from 'next/script'
+import Head from 'next/head';
+import Image from 'next/image';
+import Script from 'next/script';
 
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import styles from './layout.module.css';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
 
-const name = 'Go Balkan'
-export const siteTitle = 'GoBalkan'
+const name = 'Go Balkan';
+export const siteTitle = 'GoBalkan';
 
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
-        <meta
-          property="og:image"
-          content={`https://og-image.vercel.app/${encodeURI(
-            siteTitle
-          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
-        />
-        <meta name="og:title" content={siteTitle} />
-        <meta name="twitter:card" content="summary_large_image" />
+        <link rel='icon' href='/favicon.ico' />
+        <meta name='description' content='Learn how to build a personal website using Next.js' />
+        <meta property='og:image' content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
+        <meta name='og:title' content={siteTitle} />
+        <meta name='twitter:card' content='summary_large_image' />
       </Head>
-      <Script
-        src="https://connect.facebook.net/en_US/sdk.js"
-        strategy="lazyOnload"
-        onLoad={() =>
-          console.log(`script loaded correctly, window.FB has been populated`)
-        }
-      />
+      <Script src='https://connect.facebook.net/en_US/sdk.js' strategy='lazyOnload' onLoad={() => console.log(`script loaded correctly, window.FB has been populated`)} />
       <header className={styles.header}>
         {home ? (
           <>
-            <Image
-              priority
-              src="/images/profile.jpg"
-              className={utilStyles.borderCircle}
-              height={144}
-              width={144}
-              alt={name}
-            />
-            <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            <div className={utilStyles.headerImage}>
+              <Image priority src='/images/gobalkan-header.jpg' height={300} width={700} alt={name} />
+            </div>
+            <div className={utilStyles.welcomeCard}>
+              <Image priority src='/images/GoBalkan-colored.jpg' className={utilStyles.borderCircle} height={144} width={144} alt={name} />
+              <h1 className={utilStyles.heading2Xl}>{name}</h1>
+            </div>
           </>
         ) : (
           <>
-            <Link href="/">
-              <Image
-                priority
-                src="/images/profile.jpg"
-                className={utilStyles.borderCircle}
-                height={108}
-                width={108}
-                alt={name}
-              />
+            <Link href='/'>
+              <Image priority src='/images/profile.jpg' className={utilStyles.borderCircle} height={108} width={108} alt={name} />
             </Link>
             <h2 className={utilStyles.headingLg}>
-              <Link href="/" className={utilStyles.colorInherit}>
+              <Link href='/' className={utilStyles.colorInherit}>
                 {name}
               </Link>
             </h2>
@@ -70,9 +47,17 @@ export default function Layout({ children, home }) {
       <main>{children}</main>
       {!home && (
         <div className={styles.backToHome}>
-          <Link href="/">← Back to home</Link>
+          <Link href='/'>← Back to home</Link>
         </div>
       )}
+      <footer className={utilStyles.footer}>
+        <i>
+          Made by{' '}
+          <Link href='https://github.com/tottzi' target='_blank'>
+            Tottzi
+          </Link>
+        </i>
+      </footer>
     </div>
-  )
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
 export default function Home({ allPostsData }) {
   return (
@@ -12,35 +12,136 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>Személyautós Kaland Rally</p>
+        <span>A Te túrád, ez rólad szól!</span>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Amit kapsz:</h3>
         <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          Részvételi lehetőséget az egyik legolcsóbb, legfapadosabb és legdilisebb autóskalandhoz. <br /> GPS koordinátákat különböző látványos és érdekes helyekkel.
+          <br /> Új barátokat. Egy jó szervezést. Felejthetetlen emlékeket (amiket még évekig mutogathatsz a barátaidnak, és még lehet, hogy még álmaidban is visszatérnek 😉). <br /> Útvonaltervet. Utikönyv a fontosabb tudnivalókkal. Matricákat az autóra. Meglepetés programok.
+        </p>
+        <section className={`${utilStyles.padding1px}`}>
+          <h3>Amit nem kapsz tölünk:</h3>
+          <p>
+            Saját autót, Szervizautót, Pontós utvonalat. <br />( gps pont közti utvonalat kedved szerint variálhatód) <br />
+            Biztositást Szállodát Kaját piát ( enni, inni valót) Személyi edzőt, masszőrt, szakácsot. <br />
+            Babbysittert aki fogja a kezed……
+          </p>
+        </section>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Mennyibe fog kerülni?:</h3>
+        <p>
+          Nevezési díj: 200 euró / autó. <br />
+          Üzemanyag (kb. 3000 km) (autó és lábméret függvénye 😉).
+          <br />
+          Étel, ital. <br />
+          Szállás, ha nincs kedved végig vadkempingezni. <br />
+          Belépő néhány turista látványosságnál. (Pontosabb infó később)
+          <br />
         </p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>{title}</Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Alvás:</h3>
+        <p>
+          Leszünk völgyben, hegytetőn, és kocsmaudvaron is, vagy a semmi közepén.
+          <br />
+          Sátorban, hálózsákban, vagy autóban, vagy mehetsz szállodába, panzióba is önköltségre.
+          <br />
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Amit kérünk tőled:</h3>
+        <p>
+          Legyél normális.
+          <br />
+          Használd a fejed.
+          <br />
+          Tartsd be az általános és helyi szabályokat.
+          <br />
+        </p>
+      </section>
+      <h2>Amit nem tolerálunk: az agressziót!</h2>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Autó:</h3>
+        <p>
+          Az ideális személyautó vételára nem több mint 1000 euró. Azt, hogy mennyit költesz az autó felkészítésére, azt rád bízzuk.
+          <br />
+          Az 1000 eurós autó egy iránymutatás, nem fogjuk senki autójának lekérni a taxértékét.
+          <br />
+          Jöhetsz a már meglévő autóddal is, ha nem nagyértékű, és ha nem félted.
+          <br />
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Díjazás:</h3>
+        <p>
+          A legesek értékeljük, és díjazzuk.
+          <br />
+          Pl. legalkalmasabb, legszarabb állapotú, legegyedibb, legjobban kidekorált, ,legcukibb autó…..
+          <br />
+          Jöhetsz a már meglévő autóddal is, ha nem nagyértékű, és ha nem félted.
+          <br />
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Ez nem egy verseny!!!</h3>
+        <p>A gyors célba éréssel nem nyersz, hanem csak kevesebb időd marad a környék felfedezésére, kalandokban és élményekben gazdagodásra.</p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Történet:</h3>
+        <p>
+          Volt egyszer egy GumBall 3000, majd a csehek megcsinálták ennek a paródiáját, a Gum Balkánt, amin mi is részt vettünk már, és kedvet kaptunk egy hasonlót szervezni, kicsit magyarosra fűszerezve. A Gum Balkánról sok videót találhatsz a neten, kedvcsinálónak, és természetesen csak ajánlani
+          tudjuk őket, ha valaki őket választaná. Mi azért turistásabban nyomjuk majd. "A mienk jobb, mint az eredeti 😉"
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Magunkról:</h3>
+        <p>
+          Már több autós túrán vettünk részt, pl. Bamako, Nordcap, és több különböző erdélyi és más autós túrán. Egy Mongólia kiránduláson gondolkodtunk legutóbb, ami elmaradt a Covid és háború miatt. Így jutottunk el a csehek által szervezett Gum Balkánra, ahol ihletet kaptunk, és úgy gondoltuk,
+          hogy kis hazánk is megérett már egy hasonló, kis költségvetésű túrára.
+          <br />A mi túránkhoz nem kell terepjáró, nem kellenek milliók, hanem kis akarat, kevés pénz, és egy autónak látszó szállítóeszköz elegendő. Maximalizálva busszal jössz haza. 😉
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Adományozás:</h3>
+        <p>
+          A kis költségvetés ellenére nem kell sok pénz, ha az ember kicsit kedveskedni szeretne. Egy csoki, cukorka, vagy egy nyalóka a gyerekeknek, egy hideg sör a nyáját terelgető juhásznak a 40 fokos nyári melegben, vagy csak egy kis kutyaeledel az éppen hozzánk csapódó kóbor kutyának. De van,
+          ahol egy köszönés és pár jó szó is elég, ami még pénzbe sem kerül. 😉
+        </p>
+      </section>
+      <section className={`${utilStyles.padding1px}`}>
+        <h3>Lesz:</h3>
+        <p>
+          Turistáskodás (népszerű turistalátványosságok).
+          <br />
+          Off-road.
+          <br />
+          Urbex (elhagyott épületek, építmények).
+          <br />
+          Irodalom és népművészet.
+          <br />
+          Természet.
+          <br />
+          Érdekességek.
+          <br />
+          <br />
+          Ez tényleg rólad szól.
+          <br />
+          <br />
+          "Itt mindeki hülye? Igen, itt mindenki."
+        </p>
       </section>
     </Layout>
-  )
+  );
 }
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
+  const allPostsData = getSortedPostsData();
   return {
     props: {
-      allPostsData
-    }
-  }
+      allPostsData,
+    },
+  };
 }
