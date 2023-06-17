9afed337ce350c4f535b163f10289da6582f8533
Adam C <tottzi@gmail.com>
2023-06-17 08:31:56 +0200

    Update text

--- pages/index.js
+++ pages/index.js
@@ -18,23 +18,29 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.padding1px}`}>
         <h3>Amit kapsz:</h3>
         <p>
-          Részvételi lehetőséget az egyik legolcsóbb, legfapadosabb és legdilisebb autóskalandhoz. <br /> GPS koordinátákat különböző látványos és érdekes helyekkel.
-          <br /> Új barátokat. Egy jó szervezést. Felejthetetlen emlékeket (amiket még évekig mutogathatsz a barátaidnak, és még lehet, hogy még álmaidban is visszatérnek 😉). <br /> Útvonaltervet. Utikönyv a fontosabb tudnivalókkal. Matricákat az autóra. Meglepetés programok.
+          Részvételi lehetőséget az egyik legolcsóbb, legfapadosabb és legdilisebb autóskalandhoz.
+          <br /> GPS koordinátákat különböző látványos és érdekes helyekkel
+          <br /> Új barátokat, egy jó szervezést, felejthetetlen emlékeket (amiket még évekig mutogathatsz a barátaidnak és lehet hogy még álmaidban is visszatérnek 😉).
+          <br /> Útvonaltervet, útikönyv a fontosabb tudnivalókkal, matricákat az autóra, meglepetés programok.
         </p>
         <section className={`${utilStyles.padding1px}`}>
           <h3>Amit nem kapsz tölünk:</h3>
           <p>
-            Saját autót, Szervizautót, Pontós utvonalat. <br />( gps pont közti utvonalat kedved szerint variálhatód) <br />
-            Biztositást Szállodát Kaját piát ( enni, inni valót) Személyi edzőt, masszőrt, szakácsot. <br />
-            Babbysittert aki fogja a kezed……
+            Saját autót, szervízautót, pontos útvonalat
+            <br />
+            (GPS pontok közti útvonalat kedved szerint variálhatod).
+            <br />
+            Biztositást, szállodát, kaját- piát (enni, innivalót), személyi edzőt, masszőrt, szakácsot.
+            <br />
+            Babysittert, aki fogja a kezed…
           </p>
         </section>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Mennyibe fog kerülni?:</h3>
         <p>
-          Nevezési díj: 200 euró / autó. <br />
-          Üzemanyag (kb. 3000 km) (autó és lábméret függvénye 😉).
+          Nevezési díj: 200 EUR / autó <br />
+          Üzemanyag (kb. 2500 km, autó és lábméret függvénye 😉).
           <br />
           Étel, ital. <br />
           Szállás, ha nincs kedved végig vadkempingezni. <br />
@@ -45,7 +51,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.padding1px}`}>
         <h3>Alvás:</h3>
         <p>
-          Leszünk völgyben, hegytetőn, és kocsmaudvaron is, vagy a semmi közepén.
+          Leszünk völgyben, hegytetőn, és kocsmaudvaron is vagy a semmi közepén.
           <br />
           Sátorban, hálózsákban, vagy autóban, vagy mehetsz szállodába, panzióba is önköltségre.
           <br />
@@ -54,11 +60,7 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.padding1px}`}>
         <h3>Amit kérünk tőled:</h3>
         <p>
-          Legyél normális.
-          <br />
-          Használd a fejed.
-          <br />
-          Tartsd be az általános és helyi szabályokat.
+          Legyél normális, használd a fejed és tartsd be az általános és helyi szabályokat.
           <br />
         </p>
       </section>
@@ -66,71 +68,85 @@ export default function Home({ allPostsData }) {
       <section className={`${utilStyles.padding1px}`}>
         <h3>Autó:</h3>
         <p>
-          Az ideális személyautó vételára nem több mint 1000 euró. Azt, hogy mennyit költesz az autó felkészítésére, azt rád bízzuk.
+          Az ideális személyautó vételára nem több, mint 1000 euró. Azt, hogy mennyit költesz az autó felkészitésre, rád bízzuk.
           <br />
-          Az 1000 eurós autó egy iránymutatás, nem fogjuk senki autójának lekérni a taxértékét.
+          Az 1000 eurós autó egy iránymutatatás, nem fogjuk senki autójának lekérni a taxértékét.
           <br />
-          Jöhetsz a már meglévő autóddal is, ha nem nagyértékű, és ha nem félted.
+          Jöhetsz a már meglévő autóddal is, ha nem nagyértékű és ha nem félted.
+          <br />
+          Csak személyautók jelentkezését várjuk (nem terepjárós túra) de azért lesz off-road. 😉
           <br />
         </p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Díjazás:</h3>
         <p>
-          A legesek értékeljük, és díjazzuk.
+          A legeket értékeljük, és díjazzuk.
           <br />
-          Pl. legalkalmasabb, legszarabb állapotú, legegyedibb, legjobban kidekorált, ,legcukibb autó…..
+          Például legalkalmatlanabb, legszarabb állapotú, legegyedibb, legjobban kidekorált, legcukibb autó…
           <br />
-          Jöhetsz a már meglévő autóddal is, ha nem nagyértékű, és ha nem félted.
+          A legjobb jelmezt.
+          <br />
+          A legelső lerobbanó…és hasonlók.
           <br />
         </p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Ez nem egy verseny!!!</h3>
-        <p>A gyors célba éréssel nem nyersz, hanem csak kevesebb időd marad a környék felfedezésére, kalandokban és élményekben gazdagodásra.</p>
+        <p>A gyors célba éréssel nem nyersz, hanem csak kevesebb időd marad a környék felfedezésére, kalandokban és élményekben gazdagodni.</p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Történet:</h3>
         <p>
-          Volt egyszer egy GumBall 3000, majd a csehek megcsinálták ennek a paródiáját, a Gum Balkánt, amin mi is részt vettünk már, és kedvet kaptunk egy hasonlót szervezni, kicsit magyarosra fűszerezve. A Gum Balkánról sok videót találhatsz a neten, kedvcsinálónak, és természetesen csak ajánlani
-          tudjuk őket, ha valaki őket választaná. Mi azért turistásabban nyomjuk majd. "A mienk jobb, mint az eredeti 😉"
+          Volt egyszer egy GumBall 3000, majd a csehek megcsinálták ennek a paródiáját
+          <br />
+          a Gumbalkant, amin mi is részt vettünk már, és kedvet kaptunk egy hasonlót szervezni kicsit magyarosra fűszerezve.
+          <br />
+          A Gumbalkanról sok videót találtok a neten kedvcsinálónak, és természetesen csak ajánlani tudjuk őket, ha valaki azt választaná.
+          <br />
+          Mi azért turistásabban nyomjuk majd.
+          <br />A miénk jobb, mint az eredeti. 😉
         </p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Magunkról:</h3>
         <p>
-          Már több autós túrán vettünk részt, pl. Bamako, Nordcap, és több különböző erdélyi és más autós túrán. Egy Mongólia kiránduláson gondolkodtunk legutóbb, ami elmaradt a Covid és háború miatt. Így jutottunk el a csehek által szervezett Gum Balkánra, ahol ihletet kaptunk, és úgy gondoltuk,
-          hogy kis hazánk is megérett már egy hasonló, kis költségvetésű túrára.
-          <br />A mi túránkhoz nem kell terepjáró, nem kellenek milliók, hanem kis akarat, kevés pénz, és egy autónak látszó szállítóeszköz elegendő. Maximalizálva busszal jössz haza. 😉
+          Már több autós túrán részt vettünk, pl.: Budapest - Bamakon, megjártuk Nordcappot és több erdélyi és más autós túrát.
+          <br />
+          Egy Mongólia kiránduláson gondolkodtunk legutóbb, ami elmaradt covid és háború miatt, így jutottunk el a csehek által szervezett Gumbalkanra, ahol ihletet kaptunk és úgy gondoltuk, hogy kis hazánk is megérett már egy hasonló kis költségvetésű túrára.
+          <br />A mi túránkhoz nem kell terepjáró, nem kellenek milliók, hanem kis akarat, kevés pénz és egy autónak látszó szállítóeszköz elegendő. Max busszal jössz haza. 😉
         </p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Adományozás:</h3>
         <p>
-          A kis költségvetés ellenére nem kell sok pénz, ha az ember kicsit kedveskedni szeretne. Egy csoki, cukorka, vagy egy nyalóka a gyerekeknek, egy hideg sör a nyáját terelgető juhásznak a 40 fokos nyári melegben, vagy csak egy kis kutyaeledel az éppen hozzánk csapódó kóbor kutyának. De van,
-          ahol egy köszönés és pár jó szó is elég, ami még pénzbe sem kerül. 😉
+          A kis költségvetés ellenére nem kell sok pénz ha az ember kicsit kedveskedni szeretne. Egy hideg sör a nyáját terelgető juhásznak nyáron a 40 fokban, vagy csak egy kis kutyaeledel az éppen hozzánk csapadó kóbor kutyának.
+          <br />
+          De van ahol egy köszönés és pár jó szó is elég ami még pénzbe sem kerül. 😉
+          <br />
+          Szemétszedés. Alkalmi táborhelyeinken természetesen nem hagyunk magunk után szemetet, de ha valaki érez magában ambíciót a mások által otthagyott szemét összegyűjtésére is, ehhez szemeteszsákot és gumikesztyűt tudunk biztosítani.
         </p>
       </section>
       <section className={`${utilStyles.padding1px}`}>
         <h3>Lesz:</h3>
         <p>
-          Turistáskodás (népszerű turistalátványosságok).
+          Turistáskodás (népszerű turistalátványosságok)
           <br />
           Off-road.
           <br />
-          Urbex (elhagyott épületek, építmények).
+          Urbex (elhagyott épületek, épitmények)
           <br />
-          Irodalom és népművészet.
+          Irodalom és népmüvészet
           <br />
-          Természet.
+          Természet
           <br />
-          Érdekességek.
+          Érdekességek
           <br />
           <br />
           Ez tényleg rólad szól.
           <br />
           <br />
-          "Itt mindeki hülye? Igen, itt mindenki."
+          “Itt mindeki hülye? Igen, itt mindenki.”
         </p>
       </section>
     </Layout>
